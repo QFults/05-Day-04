09f51f11178a2e3db4a30c9e61cfe4ac67ee771d
Quinton Fults <quinton.fults.work@gmail.com>
2021-06-04 11:21:02 -0700

    code

---
+++ index.js
@@ -0,0 +1,26 @@
+function User (name, email, username) {
+  this.name = name
+  this.email = email
+  this.username = username
+  this.doSomething = function () {
+    console.log('Look what I can do!')
+  }
+}
+
+function Student (name, email, username, gpa, grade) {
+  User.call(this, name, email, username)
+  this.gpa = gpa
+  this.grade = grade
+}
+
+function Teacher (name, email, username, subject) {
+  User.call(this, name, email, username)
+  this.subject = subject
+}
+
+const student1 = new Student('Jimmy Doe', 'jimmydoe@gmail.com', 'jimmydoe', 4, 12)
+const teacher1 = new Teacher('John Doe', 'johndoe@gmail.com', 'johndoe', 'Math')
+
+console.log(student1)
+console.log(teacher1)
+
